e66ad6c242c90260f2f0b253f5ce7965855f0e49
varunkamathi <varunkamathi522004@gmail.com>
2025-01-13 11:06:23 +0530

    crete server and database

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors'; // For handling Cross-Origin requests
+import dotenv from 'dotenv';
+import connectDB from '../src/db.js';
+
+
+// Load environment variables from .env file
+dotenv.config({
+    path : "./env"
+});
+
+// Initialize the app
+const app = express();
+
+// Middleware
+const corsOptions = {
+    origin: 'http://localhost:5173', // Frontend URL
+    credentials: true, // Allow cookies to be sent
+  };
+app.use(express.json()); // Parse JSON bodies
+app.use(cors(corsOptions)); // Enable CORS for all routes
+//app.use(cookieParser());
+
+
+// Connect to MongoDB
+connectDB()
+
+
+// Basic route for testing
+// Import routes
+/*import authRoutes from './routes/auth.routes.js'; // Import the auth routes
+app.use('/api/users', authRoutes);*/
+
+
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
